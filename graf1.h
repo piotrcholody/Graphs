fe25376f642b008ee7f2e99d84eac8fe79295955
piotrcholody <piotr.cholody123@gmail.com>
2017-03-10 19:44:15 +0100

    Update graf1.h

--- graf1.h
+++ graf1.h
@@ -1,24 +1,24 @@
 #include <vector>
 #include <iostream>
-
+ 
 template <typename Type>
 class ConnectionMatrix{
 public:
     ConnectionMatrix(){
         std::cin >> *this;
     };
-
+ 
     ~ConnectionMatrix(){
     };
-
+ 
     Type& operator() (int row, int col){
         return matrix[row][col];
     };
-
+ 
     const Type& operator() (int row, int col) const{
         return matrix[row][col];
     };
-
+ 
     friend std::istream& operator>> (std::istream& is, ConnectionMatrix& obj){
         int help;
         std::cout << "Podaj rozmiar ";
@@ -34,16 +34,16 @@ public:
         }
         return is;
     };
-
+ 
     friend std::ostream& operator<< (std::ostream& os, const ConnectionMatrix& obj){
-		for(int i = 0; i < obj.size; ++i){
+        for(int i = 0; i < obj.size; ++i){
                 os << std::endl;
                 for(int j = 0; j < obj.size; ++j){
                     os << obj.matrix[i][j] << "  ";
                 }
-		}
-		return os;
-	};
+        }
+        return os;
+    };
 private:
     int size;
     std::vector< std::vector<Type> > matrix;
