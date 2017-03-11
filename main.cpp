506aa36ceb2a0fe92fb3a5c40a5ac340b2fe7df6
embchnk <baniak996@gmail.com>
2017-03-11 12:11:12 +0100

    Added header file main.h which is included in main.cpp, added few lines in main.cpp to operate on ConnectionMatrix"

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include <iostream>
-#include "AdjacencyList.h"
+#include "main.h"
 
 int main( void ){
 
-	AdjacencyList *test = new AdjacencyList();
 
+	//Testy przedstawienia grafu za pomocą macierzy połączeń:
+	ConnectionMatrix<int> *testCM = new ConnectionMatrix<int>();
+
+	std::cout << *testCM << std::endl;
+	
+	
+	//Testy przedstawienia grafu za pomocą listy:
+	AdjacencyList *test = new AdjacencyList();
 	test -> initList();
 
 	std::vector< vertex* > temp;
 	temp = test -> retAdjacencyList();
 
-	std::cout << std::endl;
+	std::cout << std::endl;	
 
 	for( int i = 0; i < ( int )temp.size(); ++i ){
 		for( int j = 0; j < ( int )temp[i] -> retSize(); ++j )
---
+++ main.h
@@ -0,0 +1,8 @@
+#ifndef main_h
+#define main_h
+#include "AdjacencyList.h"
+#include "connectionmatrix.h"
+
+
+
+#endif // main_h
