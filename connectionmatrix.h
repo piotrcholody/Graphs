f6fae1b5e253a11ba60554a5c46c058e68153e06
piotrcholody <piotr.cholody123@gmail.com>
2017-03-12 04:01:47 +0100

    Update connectionmatrix.h

--- connectionmatrix.h
+++ connectionmatrix.h
@@ -1,56 +1,88 @@
 /**
-* czubala to pedal jp2gmd
+* connection matrix
 * @author piotr cholody
 *
 */
-
+ 
 #include <vector>
 #include <iostream>
-
+ 
 template <typename Type>
-class ConnectionMatrix{
+class ConnectionMatrix {
 public:
-    ConnectionMatrix(){
+ 
+    //konstruktor
+    ConnectionMatrix()
+    {
         std::cin >> *this;
     };
- 
-    ~ConnectionMatrix(){
-    };
- 
-    Type& operator() (int row, int col){
+    //destruktor
+    ~ConnectionMatrix(){};
+    //operator zwracajacy dane pole macierzy
+    Type& operator() (int row, int col)
+    {
         return matrix[row][col];
     };
- 
-    const Type& operator() (int row, int col) const{
+    //operator zwracajacy dane pole macierzy - wersja const
+    const Type& operator() (int row, int col) const
+    {
         return matrix[row][col];
     };
- 
-    friend std::istream& operator>> (std::istream& is, ConnectionMatrix& obj){
-        int help;
-        std::cout << "Podaj rozmiar ";
+    //extraction operator
+    friend std::istream& operator>> (std::istream& is, ConnectionMatrix& obj)
+    {
+        int first, second, n;
+        std::cout << "Podaj ilosc wierzcholkow ";
         is >> obj.size;
-        for(int i = 0; i < obj.size; ++i){
-                std::vector<Type> row;
-            for(int j = 0; j < obj.size; ++j){
-                std::cout << "Podaj pole " << i << " " << j << "\n";
-                is >> help;
-                row.push_back(help);
+        if (obj.size < 1) {
+            throw std::runtime_error("graf musi miec wierzcholki");
+        }
+        std::cout << "Podaj ilosc krawedzi ";
+        is >> n;
+        if (n < 0) {
+            throw std::runtime_error("liczba krawedzi nie moze byc ujemna");
+        }
+        if (n > obj.size * (obj.size - 1)/2) {
+            throw std::runtime_error("za duza ilosc krawedzi");
+        }
+        obj.matrix.resize(obj.size);
+        for (int i = 0; i < obj.size; ++i) {
+            obj.matrix[i].resize(obj.size);
+        }
+        for (int j = 0; j < n; ++j) {
+            std::cout << "Podaj numery polaczonych wierzcholkow ";
+            is >> first;
+            if (first < 1 || first > obj.size) {
+                throw std::runtime_error("nie ma takiego wierzcholka");
             }
-            obj.matrix.push_back(row);
+            is >> second;
+            if (second < 1 || second > obj.size) {
+                throw std::runtime_error("nie ma takiego wierzcholka");
+            }
+            if (obj.matrix[first-1][second-1] == 1 || obj.matrix[second-1][first-1] == 1) {
+                throw std::runtime_error("podales dwa razy to samo polaczenie");
+            }
+            obj.matrix[first-1][second-1] = 1;
+            obj.matrix[second-1][first-1] = 1;
         }
         return is;
     };
- 
-    friend std::ostream& operator<< (std::ostream& os, const ConnectionMatrix& obj){
-        for(int i = 0; i < obj.size; ++i){
+    //insertion operator
+    friend std::ostream& operator<< (std::ostream& os, const ConnectionMatrix& obj)
+    {
+        for (int i = 0; i < obj.size; ++i) {
                 os << std::endl;
-                for(int j = 0; j < obj.size; ++j){
+                for (int j = 0; j < obj.size; ++j) {
                     os << obj.matrix[i][j] << "  ";
                 }
         }
         return os;
     };
+ 
 private:
+ 
+    //liczba wierzcholkow - rozmiar macierzy
     int size;
+    //macierz
     std::vector< std::vector<Type> > matrix;
 };
