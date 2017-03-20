952d355fb053c787bee82ad57048e48c3622d3ea
NarQiu <magma1magma@gmail.com>
2017-03-20 18:22:29 +0100

    Add files via upload

--- IncidenceMatrix.cpp
+++ IncidenceMatrix.cpp
@@ -1,83 +1,162 @@
 #include <iostream>
+#include <fstream>
 #include "IncidenceMatrix.h"
 #include "vld.h"
 
-IncidenceMatrix::IncidenceMatrix(int numberOfTops, int numberOfEdges)
-	:top(numberOfTops),
-	edge(numberOfEdges) {
-	matrix = new int*[top];
+int** IncidenceMatrix::allocateMatrix(int top, int edge) {
+	int** matrix = new int*[top];
 	for (int t = 0; t < top; t++) {
 		matrix[t] = new int[edge];
 		for (int e = 0; e < edge; e++)
 			matrix[t][e] = 0;
 	}
+	return matrix;
 }
-
-
+/******************************************************************/
+IncidenceMatrix::IncidenceMatrix(int numberOfTops, int numberOfEdges)
+	:top(numberOfTops),
+	edge(numberOfEdges) 
+{
+	matrix = allocateMatrix(numberOfTops, numberOfEdges);
+	
+}
+/******************************************************************/
 IncidenceMatrix::IncidenceMatrix()
 	:top(1),
-	edge(0) {
+	edge(0)
+{
 	while (top <= 1) {
 		std::cout << "Podaj ilosc wierzcholkow (liczbe >= 2): ";
 		cinSelectedInt(top);
-	};
+	}
 	while (edge <= 0) {
 		std::cout << "Podaj ilosc wszystkich krawedzi (liczbe >=1): ";
 		cinSelectedInt(edge);
-	};
-	matrix = new int*[top];
-	for (int t = 0; t < top; t++) {
-		matrix[t] = new int[edge];
-		for (int e = 0; e < edge; e++)
-			matrix[t][e] = 0;
 	}
+	matrix = allocateMatrix(top, edge);
 }
-
-bool IncidenceMatrix::changeTopsOfEdge(int selectedEdge, int newOwner1, int newOwner2) {
+/******************************************************************/
+bool IncidenceMatrix::setTopsOfEdge(int selectedEdge, int newOwner1, int newOwner2) {
 	if (newOwner1 < top && newOwner2 < top && newOwner1 >= 0 && newOwner2 >= 0) {
-		if (selectedEdge < edge && selectedEdge >= 0) {
-			for (int t = 0; t < top; t++)
-				matrix[t][selectedEdge] = 0;
-			matrix[newOwner1][selectedEdge] = 1;
-			matrix[newOwner2][selectedEdge] = 1;
-			return 1;
+		if (edgeIndexExist(selectedEdge)) {
+			if (newOwner1 != newOwner2) {
+				if (!isThisEdgeFree(newOwner1, newOwner2)) {
+					for (int t = 0; t < top; t++)
+						matrix[t][selectedEdge] = 0;
+					matrix[newOwner1][selectedEdge] = 1;
+					matrix[newOwner2][selectedEdge] = 1;
+					return 0;
+				}
+				else {
+					std::cout << "setTopsOfEdge(" << selectedEdge << ", " << newOwner1 << ", " << newOwner2 << "): ";
+					std::cout << "Taka krawedz juz istnieje! Nie zmieniono wierzcholkow!" << std::endl;
+				}
+			}
+			else {
+				std::cout << "setTopsOfEdge(" << selectedEdge << ", " << newOwner1 << ", " << newOwner2 << "): ";
+				std::cout << "Zadane wierzcholki maja ten sam indeks! (rowny " << newOwner1 << std::endl;
+			}
 		}
-		else return 0;
+		else {
+			std::cout << "setTopsOfEdge(" << selectedEdge << ", " << newOwner1 << ", " << newOwner2 << "): ";
+			std::cout << "Krawedz o indeksie " << selectedEdge << " nie istnieje!!!" << std::endl;
+		}
 	}
-	else return 0;
+	else {
+		std::cout << "setTopsOfEdge(" << selectedEdge << ", " << newOwner1 << ", " << newOwner2 << "): ";
+		std::cout << "Przynajmniej jeden z zadanych wierzcholkow nie istnieje!!!" << std::endl;
+	}
+	return 1;
 }
-
-bool IncidenceMatrix::addNewEdge(int newOwner1, int newOwner2) {
-	if (newOwner1 < top && newOwner2 < top && newOwner1 >=0 && newOwner2 >= 0 ) {
-		int** temp = new int*[top];
-		for (int t = 0; t < top; t++) {
-			temp[t] = new int[edge + 1];
+/******************************************************************/
+bool IncidenceMatrix::possibleEdge(int firstTop, int secondTop) {
+	return (firstTop < top && secondTop < top && firstTop >= 0 && secondTop >= 0 && firstTop != secondTop);
+}
+/******************************************************************/
+bool IncidenceMatrix::isThisEdgeFree(int firstTop, int secondTop) {
+	if (possibleEdge(firstTop, secondTop)) {
+		bool ValueOccured = false;
+		while (!ValueOccured) {
 			for (int e = 0; e < edge; e++) {
+				if (matrix[firstTop][e] == 1 && matrix[secondTop][e] == 1) {
+					ValueOccured = true;
+				}
+			}
+			break;
+		}
+		return ValueOccured;
+	}
+	else
+		return true;
+}
+/******************************************************************/
+bool IncidenceMatrix::edgeIndexExist(int questionedEdge) {
+	return (questionedEdge < edge && questionedEdge >= 0);
+}
+/******************************************************************/
+bool IncidenceMatrix::deleteEdge(int unwantedEdge) {
+	///std::cout << "deleteEdge(" << unwantedEdge << ") :" << std::endl;
+	///std::cout << "_____edgeIndexExist(unwantedEdge)==" << edgeIndexExist(unwantedEdge) << std::endl;
+	if (edgeIndexExist(unwantedEdge)) {
+		int** temp = allocateMatrix(top, edge - 1);
+		int e;
+		for (int t = 0; t < top; t++) {
+			for (e = 0; e < unwantedEdge; e++) {
 				temp[t][e] = matrix[t][e];
 			}
+			for (e = unwantedEdge; e < edge - 1; e++) {
+				temp[t][e] = matrix[t][e+1];
+			}
 			delete[] matrix[t];
-			temp[t][edge] = 0;
-			matrix[t] = temp[t];
 		}
 		delete[] matrix;
-		temp[newOwner1][edge] = 1;
-		temp[newOwner2][edge] = 1;
 		matrix = temp;
-		edge++;
+		edge--;
+		return 0;
+	}
+	else return 1;
+}
+/******************************************************************/
+bool IncidenceMatrix::addNewEdge(int newOwner1, int newOwner2) {
+	if (possibleEdge(newOwner1, newOwner2)) {
+		if (!isThisEdgeFree(newOwner1, newOwner2)) {
+			int** temp = allocateMatrix(top, edge + 1);
+			for (int t = 0; t < top; t++) {
+				for (int e = 0; e < edge; e++) {
+					temp[t][e] = matrix[t][e];
+				}
+				delete[] matrix[t];
+				temp[t][edge] = 0;
+			}
+			delete[] matrix;
+			temp[newOwner1][edge] = 1;
+			temp[newOwner2][edge] = 1;
+			matrix = temp;
+			edge++;
+			return 0;
+		}
+		else {
+			std::cout << "addNewEdge(" << newOwner1 << ", " << newOwner2 << "):";
+			std::cout << "Nie moge dodac takiej krawedzi! Taka juz istnieje!" << std::endl;
+			return 1;
+		}
+	}
+	else {
+		std::cout << "addNewEdge(" << newOwner1 << ", " << newOwner2 << "):";
+		std::cout << "Podane indeksy nie moga stworzyc krawedzi!" << std::endl;
 		return 1;
 	}
-	else return 0;
 }
-
-bool IncidenceMatrix::setEntireMatrix(){
-	std::cout<<"\nWpisuj kolejne pola oddzielając enterem \n";
-	std::cout<<"Wprowadzasz wierszami, w ktorych sa punkty \n";
-	int temp;
-	for(int t=0;t<top;t++){
-		std::cout<<"Wiersz numer "<<t+1<<": \n";
-		for(int e=0;e<edge;e++){
+/******************************************************************/
+bool IncidenceMatrix::setEntireMatrixByRows(){
+	std::cout<<"\nWpisuj kolejne pola oddzielajac enterem \n";
+	std::cout<<"Wprowadzasz wierszami (czyli 1 gdy do danego wierzcholka ma nalezec iterowana krawedz) \n";
+	int temp, t, e;
+	for (t = 0; t < top; t++) {
+		std::cout<<"Wiersz numer "<<t<<": \n";
+		for (e = 0; e < edge; e++) {
 				cinSelectedInt(temp);
-			if ((temp==0) ^ (temp==1))
+			if ((temp==0) || (temp==1))
 				matrix[t][e]=temp;
 			else{
 				std::cout<<"Mozesz wpisac tylko 0 lub 1. Wpisz liczbe ponownie: ";
@@ -86,48 +165,190 @@ bool IncidenceMatrix::setEntireMatrix(){
 		}
 		std::cout<<std::endl;
 	}
-	int counter;
-	bool initializedCorrectly = true;
-	for(int e=0;e<edge;e++){
-		counter=0;
-		for(int t=0;t<top;t++){
-			if (matrix[t][e]==1)
-				counter++;
+	bool errorOccured = eliminateAllMistakes();
+	if (errorOccured)
+		std::cout<<"\nMacierz byla zle wpisana. Naprawiono bledy."<<std::endl;
+	return errorOccured;
+}
+/******************************************************************/
+bool IncidenceMatrix::setEntireMatrixByColumns() { 
+	std::cout << "\nWpisuj kolejne pola oddzielajac enterem \n";
+	std::cout << "Wprowadzasz kolumnami (czyli 1 gdy iterowany wierzcholek ma byc poczatkiem/koncem krawedzi) \n";
+	int temp, e, t;
+	for (e = 0; e < edge; e++) {
+		std::cout << "Kolumna numer " << e << ": \n";
+		for (t = 0; t < top; t++) {
+			cinSelectedInt(temp);
+			if ((temp == 0) || (temp == 1))
+				matrix[t][e] = temp;
+			else {
+				std::cout << "Mozesz wpisac tylko 0 lub 1. Wpisz liczbe ponownie: ";
+				t--;
+			}
 		}
-		if (counter!=2){
-			std::cout<<"Przy wierzcholku E"<<e+1<<" znaleziono blad (suma kolumny musi byc 2)\n";
-			initializedCorrectly = false;
+		std::cout << std::endl;
+	}
+	bool errorOccured = eliminateAllMistakes();
+	if (errorOccured)
+		std::cout << "\nMacierz byla zle wpisana. Naprawiono bledy." << std::endl;
+	return errorOccured;
+}
+/******************************************************************/
+bool IncidenceMatrix::eliminateInvalidEdges() {
+	int counter, e, t, isnot01;
+	bool errorOccured = false;
+	if (edge > 0) {
+		for (e = edge - 1; e >= 0; e--) {
+			counter = isnot01 = 0;
+			for (t = 0; t < top; t++) {
+				if (matrix[t][e])
+					counter++;
+				if (matrix[t][e] > 1)
+					isnot01++;
+			}
+			if (counter != 2) {
+				deleteEdge(e);
+				std::cout << "Pozbyto sie krawedzi o indeksie " << e;
+				std::cout << ": nalezala do blednej liczby wierzcholkow" << std::endl;
+				errorOccured = true;
+			}
+			else if (isnot01) {
+				deleteEdge(e);
+				std::cout << "Pozbyto sie krawedzi o indeksie " << e;
+				std::cout << ": posiadala w reprezentacji liczby wieksze niz 1" << std::endl;
+				errorOccured = true;
+			}
+		}
+		if (errorOccured) 
+			std::cout << "!!!Zmieniono numeracje krawedzi z powodu usuniecia blednych krawedzi!" << std::endl;
+	}
+	return errorOccured;
+}
+/******************************************************************/
+bool IncidenceMatrix::getTopsOfEdge(int selectedEdge, int& first, int& second) {
+	if (!edgeIndexExist(selectedEdge)) {
+		std::cout << "getTopsOfEdge(" << selectedEdge << ", ..., ...): Krawedz nie istnieje!!!" << std::endl;
+		std::cout << "Pobranie wierzcholkow nie powiodlo sie!" << std::endl;
+		first = second = -2137;
+		return true;
+	}
+	else {
+		int counter = 0;
+		for (int t = 0; t < top; t++) {
+			if (matrix[t][selectedEdge]) {
+				if (counter == 0) {
+					first = t;
+					counter++;
+				}
+				else {
+					if (counter==1) {
+						second = t;
+						counter++;
+					}
+					else {
+						std::cout << "Korzystasz z popsutej macierzy incydencji! Nie mozesz pobrac wierzcholkow!" << std::endl;
+						std::abort();
+					}
+				}
+
+			}
+		}
+		return false;
+	}
+}
+/******************************************************************/
+bool IncidenceMatrix::eliminateDuplicates() {
+	bool errorOccured = false;
+	if (edge >= 1) {
+		int e, k, index1, index2, EdgeOccured;
+		for (e = edge - 1; e >= 1; e--) {
+			getTopsOfEdge(e, index1, index2);
+			k = e - 1;
+			EdgeOccured = false;
+			while (k >= 0 && !EdgeOccured) {
+				if (matrix[index1][k] == 1 && matrix[index2][k] == 1)
+					EdgeOccured = true;
+				k--;
+			}
+			if (EdgeOccured) {
+				deleteEdge(e);
+				errorOccured = true;
+				std::cout << "Usunieto zmultiplikowana krawedz o indeksie " << e << std::endl;
+			}
 		}
 	}
-	if (!initializedCorrectly)
-		std::cout<<"\nMacierz zostala zle wpisana!"<<std::endl;
-	return initializedCorrectly;
+	return errorOccured;
 }
-
-
-
+/******************************************************************/
+bool IncidenceMatrix::eliminateAllMistakes() {
+	bool x = eliminateInvalidEdges();
+	bool y = eliminateDuplicates();
+	return (x || y);
+}
+/******************************************************************/
 IncidenceMatrix::~IncidenceMatrix(){
 	for(int i=0;i<top;i++)
 		delete [] matrix[i];
 		delete [] matrix;
 }
-
+/******************************************************************/
 void IncidenceMatrix::printEntireMatrix() const{
 	std::cout<<"Macierz Indcydencji: "<<std::endl;
-	std::cout<<"    ";
-	for(int e=0;e<edge;e++) std::cout<<"E"<<e+1<<" "; std::cout<<std::endl;
-	for(int t=0;t<top;t++){
-		std::cout<<t+1<<" |";
-		for(int e=0;e<edge;e++){
-			std::cout<<" "<<matrix[t][e]<<" ";
+	if (edge) {
+		std::cout << "    ";
+		for (int e = 0; e < edge; e++) std::cout << "E" << e << " "; std::cout << std::endl;
+		for (int t = 0; t < top; t++) {
+			std::cout << t << " |";
+			for (int e = 0; e < edge; e++) {
+				std::cout << " " << matrix[t][e] << " ";
+			}
+			std::cout << "|" << std::endl;
 		}
-		std::cout<<"|"<<std::endl;
 	}
+	else std::cout << " nie posiada wierzcholkow!" << std::endl;
 }
-
-
-
-
+/******************************************************************/
+bool IncidenceMatrix::setEntireMatrixFromFile(std::string Filename, int numberOfTops, int numberOfEdges) {
+	std::ifstream mystream;
+	if (top == numberOfTops && edge == numberOfEdges) {
+		mystream.close();
+		mystream.open(Filename, std::ios::in); ///drugi argument == read-only mode
+		if (!mystream.is_open()) {
+			std::cout << "Odmowa dostepu lub nie ma pliku o tej nazwie!" << std::endl;
+			mystream.clear();
+			mystream.close();
+		}
+		else {
+			int i, j, error=0;
+			for (i = 0; i < top; i++) {
+				for (j = 0; j < edge; j++)
+					if (!(mystream >> matrix[i][j]))
+						error++;
+			}
+			if (i == top && j == edge && error == 0) {
+				std::cout << "Plik zostal wczytany poprawnie; sprawdzam bledy w reprezentacji" << std::endl;
+				if (!eliminateAllMistakes()) {
+					std::cout << "W reprezentacji nie bylo zadnych bledow" << std::endl;
+				}
+				else std::cout << "W reprezentacji byly bledy" << std::endl;
+				return 0;
+			}
+			else {
+				std::cout << "Nie ustawiono calej macierzy!" << std::endl;
+			}
+		}
+	}
+	else {
+		std::cout << "Wczytywanie z pliku nie powiodlo sie gdyz ";
+		std::cout << "zaalokowana pamiec nie zgadza sie z podanymi wymiarami" << std::endl;
+	}
+	mystream.clear();
+	mystream.close();
+	return 1;
+}
+/******************************************************************/
+/*Funkcje spoza klasy IncidenceMatrix*/
+/******************************************************************/
 
 bool cinSelectedInt(int& destination) {
 	int temp;
@@ -135,7 +356,7 @@ bool cinSelectedInt(int& destination) {
 	if (!std::cin) {
 		std::cin.clear();
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		destination = -2137;	//ustawia gdy wpiszesz napis
+		destination = -2137;
 		return false;
 	}
 	else {
--- IncidenceMatrix.h
+++ IncidenceMatrix.h
@@ -1,47 +1,95 @@
 #ifndef IncidenceMatrix_h
 #define IncidenceMatrix_h
 
-//podstawia wpisana wartosc pod int tylko wtedy gdy strumien jest dobry
-//zaleta1: nie stanie siê nic czego sie nie bedziemy spodziewac
-//zaleta2: bez dodatkowych bibliotek (tylko <iostream> potrzebny)
-//zaleta3: procesor nie robi za grzejnik na prad
-//jesli wpiszesz tekst ustawi -2137
-//jesli wpiszesz "123xxx..." to ustawi integer z poczatku strumienia
-//zwraca 1 gdy sie udalo, 0 gdy sie nie udalo
+	//podstawia wpisana wartosc pod int tylko wtedy gdy strumien jest dobry
+	//zaleta1: nie stanie siê nic czego sie nie bedziemy spodziewac
+	//zaleta2: bez dodatkowych bibliotek (tylko <iostream> potrzebny)
+	//jesli wpiszesz tekst ustawi -2137
+	//jesli wpiszesz "123xxx..." to ustawi integer z poczatku strumienia
+	//zwraca 1 gdy sie udalo, 0 gdy sie nie udalo (ustawiono -2137)
 bool cinSelectedInt(int& selectedInt);
 
 
 
-//Macierz Indcydencji
+	//Macierz Indcydencji
 class IncidenceMatrix
 {
 public:
-	//tworzy macierz pytajac o ilosc wierzcholkow i krawedzi, wypelnia zerami
+		//tworzy macierz pytajac o ilosc wierzcholkow i krawedzi, wypelnia zerami
 	IncidenceMatrix();
-	//tworzy macierz o podanym rozmiarze, wypelnia zerami
+		//tworzy macierz o podanym rozmiarze, wypelnia zerami
 	IncidenceMatrix(int numberOfTops, int numberOfEdges); 
-	//wpisujesz po kolei pola do ca³ej macierzy, wiersz po wierszu
-	//zwraca 1 gdy jest poprawnie, 0 gdy jest niepoprawnie wpisana
-	bool setEntireMatrix();
-	//wypisuje na ekran cala macierz
+		//ustawia elementy calej macierzy wpisanymi wartosciami
+		//wpisujesz po kolei pola do ca³ej macierzy, wiersz po wierszu
+		//zwraca 0 gdy jest poprawnie, 1 gdy jest niepoprawnie wpisana
+		//bledy w reprezentacji sa eliminowane; wypisywane jest co zostalo usuniete
+	bool setEntireMatrixByRows();
+		//ustawia elementy calej macierzy wpisanymi wartosciami
+		//wpisujesz po kolei pola do ca³ej macierzy, kolumna po kolumnie
+		//zwraca 0 gdy jest poprawnie, 1 gdy jest niepoprawnie wpisana
+		//bledy w reprezentacji sa eliminowane; wypisywane jest co zostalo usuniete
+	bool setEntireMatrixByColumns();
+		//ustawia elementy calej macierzy wartosciami wczytanymi z pliku 'Filename'
+		//zwraca 0 gdy wczyta poprawnie, 1 gdy wystapil jakis blad i wypisuje komunikat bledu
+		//bledy w reprezentacji sa eliminowane; wypisywane jest co zostalo usuniete
+	bool setEntireMatrixFromFile(std::string Filename, int numberOfTops, int numberOfEdges);
+		//usuwa bledy w reprezentacji macierzy incydencji, takie jak
+		//krawedzie z bledna liczba wierzcholkow lub zmultiplikowane krawedzie
+		//zwraca 1 gdy wystapily jakiekolwiek bledy (na pewno je naprawiono)
+		//zwraca 0 gdy w nie bylo bledow w reprezentacji
+		//gdy bledow nie uda sie naprawic (jakims cudem) to wczesniej wysypie sie program
+	bool eliminateAllMistakes();
+		//usuwa krawedzie ktore sie powtarzaja
+		//zwraca 0 gdy nie bylo zmultiplikowanych krawedzi
+		//nie wywoluj gdy moga istniec bledne krawedzie, nie usunie wtedy wszystkich
+	bool eliminateDuplicates();
+		//usuwa krawedzie z bledna liczba wierzcholkow lub liczbami wiekszymi niz 1
+		//zwraca 0 gdy nie bylo blednych krawedzi; zwraca 1 gdy usunieto bledne i wypisuje komunikat
+	bool eliminateInvalidEdges();
+		//inforumuje o istnieniu indeksu krawedzi (gdy istnieje zwraca 1)
+	bool edgeIndexExist(int questionedEdge);
+		//informuje o mozliwosci istnienia/stworzenia krawedzi (poprawnosc indeksow wierzcholkow)
+		//zwraca 0 gdy nie moze istniec taka krawedz
+		//zwraca 1 gdy taka krawedz moze istniec
+	bool possibleEdge(int firstTop, int secondTop);
+		//informuje o mozliwosci dodania krawedzi o zadanych indeksach wierzcholkow
+		//zwraca 0 gdy nie ma takiej krawedzi (mozna taka dodac)
+		//zwraca 1 gdy taka krawedz istnieje lub podano zle indeksy etc. (NIE mozna takiej dodac)
+	bool isThisEdgeFree(int firstTop, int secondTop);
+		//wypisuje na ekran cala macierz
 	void printEntireMatrix() const;
-	//zmienia wierzcho³ki do których nale¿y zadana krawêdŸ
-	//oczywiscie podajesz indeks tablicy (czyli dla pierwszej krawedzi/wierzcholka jest 0)
-	//gdy nowy wlasciciel lub wybrana krawedz nie istnieje funkcja zwraca 0 i nic nie robi
-	//gdy istnieja te wierzcholki i krawedzie, to zmienia i zwraca 1
-	bool changeTopsOfEdge(int selectedEdge, int newOwner1, int newOwner2);
+		//ustala(badz zmienia) wierzcho³ki do których nale¿y zadana krawêdŸ
+		//gdy ktorekolwiek indeksy sa bledne zwraca 1 i wypisuje komunikat bledu 
+		//gdy taka krawedz juz istnieje zwraca 1 i wypisuje komunikat bledu
+		//gdy operacja sie udaje zwraca 0
+	bool setTopsOfEdge(int selectedEdge, int newOwner1, int newOwner2);
+		//funckja zwraca indeksy wybranej krawedzi poprzez zmiane waratosci referowanych integerow
+		//gdy indeksy wierzcholkow zostana poprawnie pobrane i wpisane pod referowane zmienne: zwraca 0
+		//gdy indeks krawedzi jest bledny zwraca 1, a pod referowane zmienne podstawia -2137
+	bool getTopsOfEdge(int selectedEdge, int& storageInt1, int& storageInt2);
 	//dodaje nowa krawedz nalezaca do zadanych wierzcholkow (realokacja pamieci)
-	//protip: oczywiscie podajesz indeks tablicy (czyli dla pierwszego wierzcholka jest 0)
-	//gdy nowy wlasciciel nie istnieje funkcja zwraca 0 i nic nie robi
-	//gdy doda zwraca 1
+		//gdy uzyjesz zlych nieporawnych indeksow lub taka krawedz juz istnieje: zwraca 1 i wypisuje blad
+		//gdy pomyslnie doda krawedz zwraca 0
 	bool addNewEdge(int newOwner1, int newOwner2);
-	//powoduje destrukcje wszechswiata oraz usuniecie macierzy
+		//gdy krawedz nie istnieje zwraca 1
+		//gdy usunie pomyslnie zwraca 0
+	bool deleteEdge(int selectedEdge);
+		//alokuje pamiec na macierz incydencji o 'top' wierzcholkach i 'edge' krawedziach
+		//zwraca wskaznik na stworzona macierz
+	static int** IncidenceMatrix::allocateMatrix(int top, int edge);
+		//powoduje destrukcje wszechswiata oraz usuniecie macierzy
 	~IncidenceMatrix();
-
+		//zwraca liczbe wierzcholkow
+	const int getTop() const { return top; }
+		//zwraca liczbe krawedzi
+	const int getEdge() const { return edge; }
 private:
-	int top;  //wierzcholki
-	int	edge;	//krawedzie
-	int** matrix;	//macierz
+		//wierzcholki
+	int top;  
+		//krawedzie
+	int	edge;	
+		//macierz incydencji
+	int** matrix;	
 };
 
 
