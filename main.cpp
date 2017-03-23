af93033aca53165c5cb50c51178210029a7ae75a
NarQiu <magma1magma@gmail.com>
2017-03-23 10:40:50 +0100

    cofam

--- main.cpp
+++ main.cpp
@@ -3,7 +3,61 @@
 
 int main( void ){
 
+	//Testy macierzy incydencji
+	
+	
+	std::cout << "Tworzenie macierzy konstruktorem domyslnym" << std::endl;
+	IncidenceMatrix imatrix;
+	std::cout << "setEntireMatrix...() zwraca: " << imatrix.setEntireMatrixByColumns() << std::endl;
+	imatrix.printEntireMatrix();
+	std::cout << "imatrix.setTopsOfEdge(1, 0, 1) zwraca: " << imatrix.setTopsOfEdge(1, 0, 1) << std::endl;
+	imatrix.printEntireMatrix();
+	std::cout << "imatrix.setTopsOfEdge(1, 100, 99) zwraca: " << imatrix.setTopsOfEdge(1, 100, 99) << std::endl;
+	imatrix.printEntireMatrix();
+	std::cout << "imatrix.setTopsOfEdge(1, 1, 1) zwraca: " << imatrix.setTopsOfEdge(1, 1, 1) << std::endl;
+	imatrix.printEntireMatrix();
+	std::cout << "imatrix.addNewEdge(0, 1) zwraca: " << imatrix.addNewEdge(0, 1) << std::endl;
+	imatrix.printEntireMatrix();
+	std::cout << "imatrix.addNewEdge(1, 2) zwraca: " << imatrix.addNewEdge(1, 2) << std::endl;
+	imatrix.printEntireMatrix();
+	std::cout << "imatrix.addNewEdge(10, 1) zwraca: " << imatrix.addNewEdge(10, 1) << std::endl;
+	imatrix.printEntireMatrix();
+	std::cout << "imatrix.addNewEdge(0, 0) zwraca: " << imatrix.addNewEdge(0, 0) << std::endl;
+	imatrix.printEntireMatrix();
+	std::cout << "imatrix.deleteEdge(0) zwraca: " << imatrix.deleteEdge(0) << std::endl;
+	imatrix.printEntireMatrix();
+	std::cout << "imatrix.deleteEdge(100) zwraca: " << imatrix.deleteEdge(100) << std::endl;
+	imatrix.printEntireMatrix();
+	int x, y;
+	std::cout << "imatrix.getTopsOfEdge(10, x, y) zwraca: " << imatrix.getTopsOfEdge(10, x, y) << std::endl;
+	std::cout << "getTopsOfEdge(10, x, y): x: " << x << " y: " << y << std::endl;
+	std::cout << "imatrix.getTopsOfEdge(0, x, y) zwraca: " << imatrix.getTopsOfEdge(0, x, y) << std::endl;
+	std::cout << "getTopsOfEdge(0, x, y) x: " << x << " y: " << y << std::endl;
+	std::cout << "imatrix.isThisEdgeFree(0, 1) zwraca: " << imatrix.isThisEdgeFree(0, 1) << std::endl;
+	std::cout << "imatrix.isThisEdgeFree(2, 1) zwraca: " << imatrix.isThisEdgeFree(2, 1) << std::endl;
+	std::cout << "imatrix.isThisEdgeFree(2, 3) zwraca: " << imatrix.isThisEdgeFree(2, 3) << std::endl;
+	std::cout << "imatrix.isThisEdgeFree(0, 0) zwraca: " << imatrix.isThisEdgeFree(0, 0) << std::endl;
+	std::cout << "imatrix.isThisEdgeFree(100, 100) zwraca: " << imatrix.isThisEdgeFree(100, 100) << std::endl;
+	std::cout << "imatrix.isThisEdgeFree(0, 100) zwraca: " << imatrix.isThisEdgeFree(0, 100) << std::endl;
+		
+	
 
+	IncidenceMatrix lmat(4, 4);
+	lmat.setEntireMatrixFromFile("Plik1.txt", 4, 4);
+	lmat.printEntireMatrix();
+	
+	/*
+	std::cout << "Tworzenie macierzy z zadana liczba wierzcholkow i krawedzi" << std::endl;
+	IncidenceMatrix imatrix2(2, 2);
+	imatrix2.setEntireMatrix();
+	imatrix2.printEntireMatrix();
+	*/
+
+	getchar();
+	
+	
+	
+	/*
 	//Testy przedstawienia grafu za pomocą macierzy połączeń:
 	ConnectionMatrix<int> *testCM;
 	try {
@@ -32,7 +86,8 @@ int main( void ){
 
 	test -> showList();
 	
-	delete testCM;
+	//delete testCM;
 	delete test;
+	*/
 	return 0;
 }
